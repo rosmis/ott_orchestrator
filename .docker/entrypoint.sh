dc69c17b093d8aabf6a4cde67678969e34544dff
rosmis <rosmis123@gmail.com>
2026-04-16 19:21:52 +0200

    update once again on entrypoint file

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -e
 
-# # Wait for database to be ready
-until php artisan db:monitor; do
-  echo "Waiting for database connection..."
-  sleep 1
-done
+composer install --no-interaction --prefer-dist --optimize-autoloader
 
 # Run migrations
 php artisan migrate --force
 php artisan db:seed --force
-composer install
 
 exec docker-php-entrypoint "$@"

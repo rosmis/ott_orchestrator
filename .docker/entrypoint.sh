6080a133d2a0168fca92db3969d7c8a83f9a2896
rosmis <rosmis123@gmail.com>
2026-04-16 19:34:45 +0200

    php artisan key:generate missing

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
 # Run migrations
+php artisan key:generate
 php artisan migrate --force
 php artisan db:seed --force
 
